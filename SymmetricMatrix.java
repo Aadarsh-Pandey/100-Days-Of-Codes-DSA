c99670fa1d12b70da925ed319435c402e9f34e30
Adarsh Pandey <YOUR_GITHUB_EMAIL>
2026-09-20 22:56:08 +0530

    Check Symmetric Matrix

---
+++ SymmetricMatrix.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SymmetricMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of matrix: ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        // Input matrix
+        System.out.println("Enter elements of matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        boolean symmetric = true;
+
+        // Check symmetric condition
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    symmetric = false;
+                    break;
+                }
+            }
+
+            if (!symmetric) {
+                break;
+            }
+        }
+
+        // Display result
+        if (symmetric) {
+            System.out.println("Matrix is Symmetric");
+        } else {
+            System.out.println("Matrix is not Symmetric");
+        }
+
+        sc.close();
+    }
+}
